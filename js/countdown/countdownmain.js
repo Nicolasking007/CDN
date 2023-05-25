5a7a29b5117c1ef3097303e00761267eab13f936
xwj <1443733886@qq.com>
2023-05-25 10:54:10 +0800

    add：自动调整时间

--- js/countdown/countdownmain.js
+++ js/countdown/countdownmain.js
@@ -56,14 +56,25 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "October 1, 2020 00:00:00",
-	render: function(data) {
-	  var el = $(this.el);
-	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>天</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>时</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>分</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>秒</span></div>");
-	}
+    date: getUpdatedDate(),
+    render: function(data) {
+        var el = $(this.el);
+        el.empty()
+          //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+          .append("<div>" + this.leadingZeros(data.days, 2) + " <span>天</span></div>")
+          .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>时</span></div>")
+          .append("<div>" + this.leadingZeros(data.min, 2) + " <span>分</span></div>")
+          .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>秒</span></div>");
+    }
 });
+
+function getUpdatedDate() {
+    var now = new Date();
+    // 检查是否超过了10月1日，如果是，则将日期加1年
+	if (now.getMonth() >= 9 && now.getDate() >= 1) {
+		now.setFullYear(now.getFullYear() + 1);
+	  }
+	// 设置倒计时的日期为10月1日，加上当前年份或者下一年份
+	var countdownDate = "October 1, " + now.getFullYear() + " 00:00:00";  
+    return countdownDate;
+}
