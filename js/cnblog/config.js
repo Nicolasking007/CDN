1e5eaa4823c4283c61eb4c768ce5544a34ef4fc8
Nicolas-kings <nicolas-kings@qq.com>
2024-04-11 18:30:32 +0800

    fix

---
+++ js/cnblog/config.js
@@ -0,0 +1,84 @@
+<script type="text/javascript">
+	window.cnblogsConfig = {
+		info: {
+			name: 'Nicolas-kings',
+			// 您的用户名
+			startDate: '2024-04-09',
+			// 您的入园时间，年-月-日。入园时间查看方法：鼠标停留园龄时间上，会显示入园时间
+			avatar: 'https://nkupp.com/images/avatar.png',
+			// 您的头像 URL 地址
+		},
+		sidebar: {
+			infoBackground: 'https://s2.ax1x.com/2019/12/15/QWO0rd.jpg',
+			titleMsg: '欢迎访问本博客~',
+			submenu: {
+				// 积分排行
+				pointsRank: true,
+				// 最新随笔
+				latestPosts: true,
+				// 我的标签
+				myTags: true,
+				// 随笔分类
+				postsClassify: true,
+				// 文章分类
+				articleClassify: true,
+				// 阅读排行
+				readRank: false,
+				// 推荐排行
+				recommendRank: false,
+				// 帖子档案
+				postsArchive: true,
+				// 文章档案
+				articleArchive: true,
+				// 自定义列表
+				customList: true,
+				// 最新评论
+				latestComment: true,
+			},
+			navList: [
+                ['首页', 'https://www.cnblogs.com/nkupp', 'icon-home'], 
+                ['github', 'https://github.com/Nicolasking007', 'icon-github'], 
+                ['博客', 'https://nkupp.com', 'icon-renzhenghuizhang'], 
+            ],
+		},
+		banner: {
+			home: {
+				title: '每一个不曾起舞的日子，都是对生命的辜负。',
+				background: [
+                    "https://s2.ax1x.com/2019/12/15/QWXgT1.jpg", 
+                    "https://s2.ax1x.com/2019/12/15/QWX5lD.jpg", 
+                ],
+			},
+			article: {
+				background: [
+                    "https://s2.ax1x.com/2019/12/15/QfZ55T.jpg", 
+                    "https://ae01.alicdn.com/kf/Uc27537fbd50e46709defa34c7edd7fcar.jpg", 
+                ],
+			},
+		},
+		code: {
+			type: 'hljs',
+			options: {
+				hljs: {
+					theme: 'atom-one-dark-reasonable',
+					languages: ['Bash', 'CSS', 'Dockerfile', 'Go', 'HTML', 'XML', 'HTTP']
+				},
+			},
+		},
+		footer: {
+			text: {
+				left: '啊哈哈哈哈哈哈哈',
+				iconFont: {
+					icon: "icon-xl",
+					color: "red",
+					fontSize: "16px",
+				},
+			},
+			aplayer: {
+				enable: false,
+			},
+		},
+	}
+</script>
+
+
